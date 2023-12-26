5e4553284e5328a9110b39730f8072b813d6f33e
Cesar Franco <cesarfrancopro@gmail.com>
2023-12-26 05:41:03 -0600

    added start countdown timer and game over screen

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 from pygame.sprite import Group
 import random
+pygame.init()
 
 # fps
 clock = pygame.time.Clock()
@@ -13,21 +14,34 @@ screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Space Race')
 
+# fonts
+font30 = pygame.font.SysFont('Constantia', 30)
+font40 = pygame.font.SysFont('Constantia', 40)
+
 # define game variables
 rows = 5
 cols = 5
 alien_cooldown = 1000 # MS
 last_alien_shot = pygame.time.get_ticks()
+countdown = 3
+last_count = pygame.time.get_ticks()
+game_over = 0
 
 # define colors
 red = (255, 0, 0)
 green = (0, 255, 0)
+white = (255, 255, 255)
 
 # load background
 bg = pygame.image.load("images/background.png")
 
 def draw_bg():
     screen.blit(bg, (0, 0))
+
+# create text
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
 
 # create superman class
 class Superman(pygame.sprite.Sprite):
@@ -46,6 +60,7 @@ class Superman(pygame.sprite.Sprite):
 
         # cooldown
         cooldown = 500 # MS
+        game_over = 0
 
         # get key press
         key = pygame.key.get_pressed()
@@ -70,7 +85,10 @@ class Superman(pygame.sprite.Sprite):
         pygame.draw.rect(screen, red, (self.rect.x, (self.rect.bottom + 10), self.rect.width, 15))
         if self.health_remaining > 0:
             pygame.draw.rect(screen, green, (self.rect.x, (self.rect.bottom + 10), int(self.rect.width * (self.health_remaining / self.health_start)), 15))
-
+        elif self.health_remaining <= 0:
+            self.kill()
+            game_over = -1
+        return game_over
 
 # create laser class
 class Lasers(pygame.sprite.Sprite):
@@ -149,32 +167,54 @@ while run:
     # draws background
     draw_bg()
 
-    # random alien lasers
-    time_now = pygame.time.get_ticks()
-    if time_now - last_alien_shot > alien_cooldown and len(alien_laser_group) < 5 and len(alien_group) > 0:
-        hostile_alien = random.choice(alien_group.sprites())
-        alien_laser = Alien_Lasers(hostile_alien.rect.centerx, hostile_alien.rect.bottom)
-        alien_laser_group.add(alien_laser)
-        last_alien_shot = time_now
+    if countdown == 0:
+        # random alien lasers
+        time_now = pygame.time.get_ticks()
+        if time_now - last_alien_shot > alien_cooldown and len(alien_laser_group) < 5 and len(alien_group) > 0:
+            hostile_alien = random.choice(alien_group.sprites())
+            alien_laser = Alien_Lasers(hostile_alien.rect.centerx, hostile_alien.rect.bottom)
+            alien_laser_group.add(alien_laser)
+            last_alien_shot = time_now
 
-    # event handlers
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+        # check if all aliens are killed
+        if len(alien_group) == 0:
+            game_over = 1
 
-    # update superman
-    superman.update()
+        if game_over == 0:
 
-    # update sprite 
-    laser_group.update()
-    alien_group.update()
-    alien_laser_group.update()
+            # update superman
+            game_over = superman.update()
+
+            # update sprite 
+            laser_group.update()
+            alien_group.update()
+            alien_laser_group.update()
+        else:
+            if game_over == -1:
+                draw_text('GAME OVER', font40, white, int(screen_width / 2 - 100), int(screen_height / 2 + 50))
+            if game_over == 1:
+                draw_text('WINNER!!!', font40, white, int(screen_width / 2 - 100), int(screen_height / 2 + 50))
+
+    
+    if countdown > 0:
+        draw_text('STARTING IN', font40, white, int(screen_width / 2 - 110), int(screen_height / 2 + 50))
+        draw_text(str(countdown), font40, white, int(screen_width / 2 - 10), int(screen_height / 2 + 100))
+        count_timer = pygame.time.get_ticks()
+        if count_timer - last_count > 1000:
+            countdown -= 1
+            last_count = count_timer
+
 
     # draw sprites
     superman_group.draw(screen)
     laser_group.draw(screen)
     alien_group.draw(screen)
     alien_laser_group.draw(screen)
+
+    # event handlers
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
     pygame.display.update()
 
